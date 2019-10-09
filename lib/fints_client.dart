d53c799b984c2210c35fedd7d2a25f6bc757e172
Stephan Vedder <vedder@mbits.info>
2019-10-09 10:50:38 +0200

    Some improvements

--- lib/fints_client.dart
+++ lib/fints_client.dart
@@ -6,15 +6,11 @@ export 'src/segments/hksyn.dart';
 /// A FinTS Client.
 class Client {
   String _errorMsg;
-  String _productId;
-  String _version;
+  var _productId = "9FA6681DEC0CF3046BFC2F8A6";
+  var _version = "1.0.0";
 
-  Client() {
-    _productId = "9FA6681DEC0CF3046BFC2F8A6";
-    _version = "1.0.0";
-  }
-
-  Client.Versioned(this._productId, this._version);
+  Client(this._productId, this._version);
+  Client.unversioned();
 
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
--- test/fints_client_test.dart
+++ test/fints_client_test.dart
@@ -1,10 +1,10 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 import 'package:fints_client/fints_client.dart';
 
 void main() {
   test('adds one to input values', () {
-    final client = Client();
+    final client = Client.unversioned();
     expect(client.addOne(2), 3);
     expect(client.addOne(-7), -6);
     expect(client.addOne(0), 1);
